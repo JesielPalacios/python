ae3aeb9ede376d6292e522cfab844fbaebe3e509
Jesiel Palacios <jopalacioss@unac.edu.co>
2022-11-30 20:43:49 -0500

    Booleans

---
+++ 6_booleans.py
@@ -0,0 +1,12 @@
+# Booleans: variables only with values true or false.
+
+is_single = True
+print(type(is_single))
+is_single = False
+print(is_single)
+
+print(not True)
+print(not False)
+
+is_single = not is_single
+print(is_single)
