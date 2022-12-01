6d6ddb9e320749937440a1d725ca3eda26568908
Jesiel Palacios <jopalacioss@unac.edu.co>
2022-11-30 21:58:18 -0500

    Operators

---
+++ fundamentals/8_arimetic_operators.py
@@ -0,0 +1,29 @@
+# (+) Suma
+# (-) Resta
+# (*) Multiplicación
+# (/) División
+# (%) Modulo o Residuo
+# (//) Division con valor entero
+# (**) Exponenciación
+
+print(10 + 10)
+print(10 - 5)
+print(10 * 2)
+print(10 / 2)  # 5
+print(10 % 2)  # 0
+print(10 / 3)
+print(10 % 3)
+print(10 // 3) # división con resultado entero.
+print(2 ** 3)
+print(2 ** 3 + 3 - 7 / 1 // 4)
+print(2 ** 3)
+print((7 / 1) // 4)
+
+print(8 + 3 - 1)
+print(10 / 0)
+
+print('Hola' + ' mundo')
+print('Hola' * 3)
+
+# Python operators
+# https://www.w3schools.com/python/python_operators.asp
