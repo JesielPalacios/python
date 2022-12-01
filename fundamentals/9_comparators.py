288d56aa9d1a04a9e0e7b3ba6350a1055494975e
Jesiel Palacios <jopalacioss@unac.edu.co>
2022-11-30 22:05:09 -0500

    Comparators

---
+++ fundamentals/9_comparators.py
@@ -0,0 +1,32 @@
+# Comparators
+# https://www.w3schools.com/python/python_operators.asp
+
+# https://www.freecodecamp.org/espanol/news/operadores-basicos-en-python-con-ejemplos/
+
+# Operadores de comparación
+
+# Estos operadores devuelven tipos de datos <boole>
+
+# ( > / < ) Mayor / Menor
+# ( >= / <= ) Mayor o igual / Menor o igual
+# ( == ) Igual
+# ( != ) Diferente
+
+
+# Ejemplos:
+
+# ( 2 > 1 ) --> True
+# ( 2 > 3 ) --> False
+# (5 > 5 ) --> False
+# (1 < 2) --> True
+# ( 2 >= 1 ) --> True
+# ( 2 >= 3 ) --> False
+# (1 <= 2) --> True
+# (5 >= 5 ) -> True
+# (4 == 4) --> True
+# (‘Apple’ == ‘Apple’) --> True
+# (‘Apple’ == ‘apple’) --> False
+# (4 == 4) --> False
+# (‘Apple’ == ‘Apple’) --> False
+# (‘Apple’ == ‘apple’) --> True
+
